6e0955cc1405760d0ab6f53b79abd3d6efbaccda
MSI\miyam <21c1020048mm@edu.tech.ac.jp>
2022-05-27 14:19:32 +0900

    ロード

--- Assets/Scripts/Scene/TitletoWepons.cs
+++ Assets/Scripts/Scene/TitletoWepons.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitletoWepons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+                SceneManager.LoadScene("WeponsScene");
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
