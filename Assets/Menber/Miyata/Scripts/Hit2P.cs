cd9a9434ae1b438f31839f5ada8effb124a8d967
MSI\miyam <21c1020048mm@edu.tech.ac.jp>
2022-06-10 10:40:27 +0900

    少し修正しました。

---
+++ Assets/Menber/Miyata/Scripts/Hit2P.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit2P : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10.0f))
+        {
+            Debug.Log(hit.collider.gameObject.transform.position);
+        }
+    }
+}
