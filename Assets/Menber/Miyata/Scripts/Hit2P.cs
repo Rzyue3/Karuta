f5ac1e759d62be4d9147e902b4264a5dd3355152
MSI\miyam <21c1020048mm@edu.tech.ac.jp>
2022-06-17 13:17:36 +0900

    2Pの当たり判定

--- Assets/Menber/Miyata/Scripts/Hit2P.cs
+++ Assets/Menber/Miyata/Scripts/Hit2P.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Hit2P : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameObject _pos;
+    private float rayDistance;
+    public GameObject atari;
+    Hit script;
+
+    private void Start()
     {
-        
+        rayDistance=50.0f; //レイの長さ
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var direction = transform.forward;
+
+        if (Input.GetMouseButtonDown(1)) //右クリックされたとき
+        {
+
+            Ray ray = new Ray(_pos.transform.position, direction); //標準の座標
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {    
+                if (hit.collider.CompareTag("Karuta")) // タグを比較
+                {
+                    script = hit.collider.GetComponent<Hit>();
+                    script.Damage();
+                    //Destroy(hit.collider.gameObject); // オブジェクトを破壊
+                }
+            }
+        }
     }
 }
+
+    
