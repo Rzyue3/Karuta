ec5fc676d41c4783eb2dafda79d34c2faf775a4c
MSI\miyam <21c1020048mm@edu.tech.ac.jp>
2022-06-24 17:12:23 +0900

    武器選択画面

--- Assets/Menber/Miyata/Scripts/Load.cs
+++ Assets/Menber/Miyata/Scripts/Load.cs
@@ -7,22 +7,56 @@ public class Load : MonoBehaviour
 {
     [SerializeField] private GameObject _loadingUI;
     [SerializeField] private Slider _slider;
-    public void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            _loadingUI.SetActive(true);
-            StartCoroutine(LoadScene());
 
-        }
+    public int ready1, ready2;
+
+    void Start()
+    {
+        ready1 = 0; //ready1‚ğ‰Šú‰»
+        ready2 = 0; //ready2‚ğ‰Šú‰»
+
     }
     IEnumerator LoadScene()
     {
-        AsyncOperation async = SceneManager.LoadSceneAsync("MainGameScene");
+        AsyncOperation async = SceneManager.LoadSceneAsync("MainGameScene"); 
         while (!async.isDone)
         {
             _slider.value = async.progress;
             yield return null;
+        }
+    }
+    public void Ready()
+    {
+        if(ready1!=0&&ready2!=0)
+        {
+            _loadingUI.SetActive(true);
+            StartCoroutine(LoadScene());
+        }
+        
+    }
+    public void Speed()
+    {
+        if(ready1==0)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                ready1 = 1;
+            }
+        }
+    }
+    public void Balance()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            ready1 = 2;
+        }
+
+    }
+    public void Power()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            ready1 = 3;
         }
     }
 }
