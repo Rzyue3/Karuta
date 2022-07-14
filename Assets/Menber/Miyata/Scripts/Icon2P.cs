2624c9bfb2d1525f6f1a27e28a67871a0813532e
Rzyue3 <kenshin.tsushima@gmail.com>
2022-07-15 07:15:15 +0900

    シーンの微調整

--- Assets/Menber/Miyata/Scripts/Icon2P.cs
+++ Assets/Menber/Miyata/Scripts/Icon2P.cs
@@ -14,25 +14,35 @@ public class Icon2P : MonoBehaviour
     [SerializeField] GameObject  Speed2;
     [SerializeField] GameObject  Balance2;
     [SerializeField] GameObject  Power2; 
-
+    [SerializeField] GameObject Speedobj;
+    [SerializeField] GameObject Balanceobj;
+    [SerializeField] GameObject Powerobj;
 
     int selectCharaNumber;
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag==Speedtag)
+        if(other.gameObject == Speedobj)
         {
-             selectCharaNumber = 1; //スピードが押されたとき1を代入
-             Speed2.gameObject.SetActive(true);
+            selectCharaNumber = 1; //スピードが押されたとき1を代入
+            Speed2.gameObject.SetActive(true);
+            Balance2.gameObject.SetActive(false);
+            Power2.gameObject.SetActive(false);
+
         }
-        if(other.gameObject.tag==Balancetag)
+        else if(other.gameObject == Balanceobj)
         {
             selectCharaNumber = 2; //バランスが押されたとき2を代入
+            Speed2.gameObject.SetActive(false);
             Balance2.gameObject.SetActive(true);
+            Power2.gameObject.SetActive(false);
+
         }
-        if(other.gameObject.tag==Powertag)
+        else if(other.gameObject == Powerobj)
         {
             selectCharaNumber = 3; //パワーが押されたとき3を代入
+            Speed2.gameObject.SetActive(false);
+            Balance2.gameObject.SetActive(false);
             Power2.gameObject.SetActive(true);
         }
         if(other.gameObject.tag==Readytag)
--- Assets/Menber/Miyata/Scripts/Test.cs
+++ Assets/Menber/Miyata/Scripts/Test.cs
@@ -12,28 +12,39 @@ public class Test : MonoBehaviour
     private string Readytag="readytag";
     [SerializeField] GameObject  Speed;
     [SerializeField] GameObject  Balance;
-    [SerializeField] GameObject  Power; 
+    [SerializeField] GameObject  Power;
+    [SerializeField] GameObject Speedobj;
+    [SerializeField] GameObject Balanceobj;
+    [SerializeField] GameObject Powerobj;
 
     int selectCharaNumber;
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag==Speedtag)
+        if(other.gameObject == Speedobj)
         {
             selectCharaNumber = 1; //スピードが押されたとき1を代入
             Speed.gameObject.SetActive(true);
+            Balance.gameObject.SetActive(false);
+            Power.gameObject.SetActive(false);
+
         }
-        if(other.gameObject.tag==Balancetag)
+        else if(other.gameObject == Balanceobj)
         {
             selectCharaNumber = 2; //バランスが押されたとき2を代入
+            Speed.gameObject.SetActive(false);
             Balance.gameObject.SetActive(true);
+            Power.gameObject.SetActive(false);
+
         }
-        if(other.gameObject.tag==Powertag)
+        else if(other.gameObject == Powerobj)
         {
             selectCharaNumber = 3; //パワーが押されたとき3を代入
+            Speed.gameObject.SetActive(false);
+            Balance.gameObject.SetActive(false);
             Power.gameObject.SetActive(true);
         }
-        if(other.gameObject.tag==Readytag)
+        else if(other.gameObject.tag==Readytag)
         {
             SceneManager.LoadScene("MainGameScene");
         }
