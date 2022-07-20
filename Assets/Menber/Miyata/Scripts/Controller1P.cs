920e90ddf25d7e755c730895a2730f4628fe6148
Rzyue3 <kenshin.tsushima@gmail.com>
2022-07-20 22:37:01 +0900

    作業途中:リザルト、マグ

--- Assets/Menber/Miyata/Scripts/Controller1P.cs
+++ Assets/Menber/Miyata/Scripts/Controller1P.cs
@@ -16,9 +16,16 @@ public class Controller1P : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        gameObject.transform.position += new Vector3(x * Speed,  -y * Speed,0);
+        var posX = x * Time.deltaTime * Speed;
+        var posY = -y * Time.deltaTime * Speed;
+        var min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        var max = Camera.main.ViewportToWorldPoint(Vector2.one);
+        gameObject.transform.position = new Vector3(
+            Mathf.Clamp( gameObject.transform.position.x + posX, min.x, max.x ),
+            Mathf.Clamp( gameObject.transform.position.y + posY, min.y, max.y ),
+            0.0f
+        );
     }
 }
--- Assets/Menber/Miyata/Scripts/Gamepad.cs
+++ Assets/Menber/Miyata/Scripts/Gamepad.cs
@@ -19,6 +19,14 @@ public class Gamepad : MonoBehaviour
     
         float x = Input.GetAxis("Horizontal2");
         float y = Input.GetAxis("Vertical2");
-        gameObject.transform.position += new Vector3(x * Speed2,  -y * Speed2,0);
+        var posX = x * Time.deltaTime * Speed2;
+        var posY = -y * Time.deltaTime * Speed2;
+        var min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        var max = Camera.main.ViewportToWorldPoint(Vector2.one);
+        gameObject.transform.position = new Vector3(
+            Mathf.Clamp( gameObject.transform.position.x + posX, min.x, max.x ),
+            Mathf.Clamp( gameObject.transform.position.y + posY, min.y, max.y ),
+            0.0f
+        );
     }
 }
